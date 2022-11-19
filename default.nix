4927aa963b4d79166100d82590a3e4510f6eca06
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-19 21:31:10 +0100

    Update nix

--- default.nix
+++ default.nix
@@ -3,13 +3,10 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
-  mutwo-core = import (mutwo-core-archive + "/default.nix");
-
   mutwo-timeline-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.timeline/archive/295b9b6ef5ec5099fc940962513b5f30d284f9a0.tar.gz";
   mutwo-timeline = import (mutwo-timeline-archive + "/default.nix");
 
-  mutwo-abjad-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.abjad/archive/0cd25e7e87ae61ebfb0343a59baf8b8564c24fca.tar.gz";
+  mutwo-abjad-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.abjad/archive/8d51930bd811796938238169884596c0abb17bcd.tar.gz";
   mutwo-abjad = import (mutwo-abjad-archive + "/default.nix");
 
   treelib = pkgs.python310Packages.buildPythonPackage rec {
@@ -31,15 +28,14 @@ in
     src = fetchFromGitHub {
       owner = "levinericzimmermann";
       repo = name;
-      rev = "6bd6aa841d79fc7a7c46e16b41bf843d9e93813a";
-      sha256 = "sha256-xLmC2+9bDwxAZ6yQ6P1y8umSkIuPerpD4kAwjp3fz84=";
+      rev = "970c8f6550a99144b6902956bc30d70734310dd3";
+      sha256 = "sha256-O4x5jL92RXylSSTbD2XWOeEDqza4CBQ7ACbh20Vw5S0=";
     };
     checkInputs = [
       python310Packages.pytest
       lilypond-with-fonts
     ];
     propagatedBuildInputs = [ 
-      mutwo-core
       mutwo-timeline
       mutwo-abjad
       lilypond-with-fonts
