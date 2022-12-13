e93edf51af616e0afcc5310870c6f3be4d34ecc2
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-12-14 00:34:07 +0100

    Add make_complex_event_to_abjad_container (don't hard code default abjad converter)

--- mutwo/clock_converters/configurations.py
+++ mutwo/clock_converters/configurations.py
@@ -1,49 +1,8 @@
-import abjad
-
-from mutwo import abjad_converters
-from mutwo import core_converters
-from mutwo import core_events
-
-
-def _event_to_time_signature_tuple(
-    event: core_events.abc.Event,
-) -> tuple[abjad.TimeSignature, ...]:
-    return getattr(event, "time_signature_tuple", None)
-
-
-class _SequentialEventToAbjadStaff(abjad_converters.SequentialEventToAbjadVoice):
-    import abjad as _abjad
-
-    def convert(self, *args, **kwargs) -> abjad.Staff:
-        voice = super().convert(*args, **kwargs)
-        voice._consists_commands = []
-        first_leaf = self._abjad.select.leaves(voice)[0]
-        self._abjad.attach(
-            self._abjad.LilyPondLiteral(
-                r"\omit Stem \omit Flag "
-                r"\omit Beam \override NoteHead.duration-log = 2"
-            ),
-            first_leaf,
-        )
-        return self._abjad.Staff([voice])
+from mutwo import clock_generators
 
 
 DEFAULT_COMPLEX_EVENT_TO_ABJAD_CONTAINER = (
-    abjad_converters.NestedComplexEventToAbjadContainer(
-        abjad_converters.CycleBasedNestedComplexEventToComplexEventToAbjadContainers(
-            (
-                _SequentialEventToAbjadStaff(
-                    abjad_converters.LeafMakerSequentialEventToDurationLineBasedQuantizedAbjadContainer(
-                        event_to_time_signature_tuple=_event_to_time_signature_tuple
-                    ),
-                    # Default case: we don't print any tempo
-                    tempo_envelope_to_abjad_attachment_tempo=None,
-                ),
-            )
-        ),
-        abjad_container_class=abjad.StaffGroup,
-        lilypond_type_of_abjad_container="StaffGroup",
-    )
+    clock_generators.make_complex_event_to_abjad_container()
 )
 
-del abjad, abjad_converters, core_events, core_converters, _SequentialEventToAbjadStaff
+del clock_generators
--- mutwo/clock_generators/__init__.py
+++ mutwo/clock_generators/__init__.py
@@ -1,12 +1,14 @@
+from .abjad import *
 from .pickers import *
 from .clock_trees import *
 
+from . import abjad
 from . import clock_trees
 from . import pickers
 
 from mutwo import core_utilities
 
-__all__ = core_utilities.get_all(pickers, clock_trees)
+__all__ = core_utilities.get_all(pickers, clock_trees, abjad)
 
 # Force flat structure
-del core_utilities, pickers, clock_trees
+del core_utilities, pickers, clock_trees, abjad
