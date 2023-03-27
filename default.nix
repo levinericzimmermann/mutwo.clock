08dd8fce747886aec44e22ec429d56eabb3af3c2
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-27 22:10:30 +0200

    nix: Use mutwo-nix, but with local src

--- default.nix
+++ default.nix
@@ -1,51 +1,10 @@
-with import <nixpkgs> {};
-with pkgs.python310Packages;
-
+{ sources ? import ./nix/sources.nix}:
 let
-
-  mutwo-timeline-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.timeline/archive/cef411566da9cff644066eceb1fb459ecaca2487.tar.gz";
-  mutwo-timeline = import (mutwo-timeline-archive + "/default.nix");
-
-  mutwo-abjad-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.abjad/archive/3ea5a58a7d4639bd820641d8efc80e106f4bfea7.tar.gz";
-  mutwo-abjad = import (mutwo-abjad-archive + "/default.nix");
-
-  treelib = pkgs.python310Packages.buildPythonPackage rec {
-    name = "treelib";
-    src = fetchFromGitHub {
-      owner = "caesar0301";
-      repo = name;
-      rev = "12d7efd50829a5a18edaab01911b1e546bff2ede";
-      sha256 = "sha256-QGgWsMfPm4ZCSeU/ODY0ewg1mu/mRmtXgHtDyHT9dac=";
-    };
-    doCheck = true;
-    propagatedBuildInputs = [ python310Packages.future ];
-  };
-
+  mutwo-clock = import (sources.mutwo-nix.outPath + "/mutwo.clock/default.nix") {};
+  mutwo-clock-local = mutwo-clock.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
-
-  buildPythonPackage rec {
-    name = "mutwo.clock";
-    src = fetchFromGitHub {
-      owner = "levinericzimmermann";
-      repo = name;
-      rev = "5f90ef0224a4e04ad792c121f7b76abc7c3363db";
-      sha256 = "sha256-7pZEO0BGImx89s+ASNu4p8pxB8t1U4jYE5wwBkSoJXc=";
-    };
-    checkInputs = [
-      python310Packages.pytest
-      lilypond-with-fonts
-    ];
-    propagatedBuildInputs = [ 
-      mutwo-timeline
-      mutwo-abjad
-      lilypond-with-fonts
-      treelib
-      python310Packages.numpy
-    ];
-    checkPhase = ''
-      runHook preCheck
-      pytest
-      runHook postCheck
-    '';
-    doCheck = true;
-  }
+  mutwo-clock-local
