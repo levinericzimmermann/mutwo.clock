2992bd09eeb98a3f0900d75e42bc0d3c134d7a19
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-05-13 13:19:52 +0200

    Allow higher mutwo.timeline version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     install_requires=[
         "mutwo.core>=1.2.0, <2.0.0",
         "mutwo.music>=0.20.0, <1.0.0",
-        "mutwo.timeline>=0.3.0, <0.4.0",
+        "mutwo.timeline>=0.3.0, <0.5.0",
         "mutwo.abjad>=0.15.0, <1.0.0",
         "numpy>=1.18, <2.00",
         "jinja2>=3.1.2, <4.0.0",
